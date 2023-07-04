ce8b69f7142a6de8aef43d1d4b72d76cb4c58436
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-07-04 15:37:09 -0300

    3. Crie testes para a função show_preview

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,36 @@
 from pro_filer.actions.main_actions import show_preview  # NOQA
+
+mock_context = {
+    "all_files": [
+        "src/dir1/file1.py",
+        "src/dir2/file2.py",
+        "src/dir3/file3.py",
+        "src/dir4/file4.py",
+        "src/dir5/file5.py",
+    ],
+    "all_dirs": [
+        "src/dir1/",
+        "src/dir2/",
+        "src/dir3/",
+        "src/dir4/",
+        "src/dir5/",
+    ],
+}
+
+mock_context_empty = {"all_files": [], "all_dirs": []}
+
+
+def test_show_preview_not_empty_context(capsys):
+    show_preview(mock_context)
+    captured = capsys.readouterr()
+    assert captured.out == str(
+        mock_context["all_files"], mock_context["all_dirs"]
+    )
+
+
+def test_show_preview_empty_context(capsys):
+    show_preview(mock_context_empty)
+    captured = capsys.readouterr()
+    assert captured.out == str(
+        mock_context_empty["all_files"], mock_context_empty["all_dirs"]
+    )
