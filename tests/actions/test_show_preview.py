5a9a81d70ecda4b55720f2e52db6f86c84d2ee7e
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-07-05 16:53:08 -0300

    Atualização do test_show_preview.py

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -2,19 +2,14 @@ from pro_filer.actions.main_actions import show_preview  # NOQA
 
 mock_context = {
     "all_files": [
-        "src/dir1/file1.py",
-        "src/dir2/file2.py",
-        "src/dir3/file3.py",
-        "src/dir4/file4.py",
-        "src/dir5/file5.py",
+        "src/__init__.py",
+        "src/app.py",
+        "src/utils/__init__.py",
+        "src/utils/alpha.py",
+        "src/utils/beta.py",
+        "src/utils/gamma.py",
     ],
-    "all_dirs": [
-        "src/dir1/",
-        "src/dir2/",
-        "src/dir3/",
-        "src/dir4/",
-        "src/dir5/",
-    ],
+    "all_dirs": ["src", "src/utils"],
 }
 
 mock_context_empty = {"all_files": [], "all_dirs": []}
@@ -23,14 +18,16 @@ mock_context_empty = {"all_files": [], "all_dirs": []}
 def test_show_preview_not_empty_context(capsys):
     show_preview(mock_context)
     captured = capsys.readouterr()
-    assert captured.out == str(
-        mock_context["all_files"], mock_context["all_dirs"]
+    assert (
+        captured.out
+        == "Found 6 files and 2 directories\n\
+First 5 files: ['src/__init__.py', 'src/app.py', 'src/utils/__init__.py',\
+ 'src/utils/alpha.py', 'src/utils/beta.py']\n\
+First 5 directories: ['src', 'src/utils']\n"
     )
 
 
 def test_show_preview_empty_context(capsys):
     show_preview(mock_context_empty)
     captured = capsys.readouterr()
-    assert captured.out == str(
-        mock_context_empty["all_files"], mock_context_empty["all_dirs"]
-    )
+    assert captured.out == "Found 0 files and 0 directories\n"
